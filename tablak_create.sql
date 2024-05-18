664357e0507e4669951e238f866f8630ba299f63
GoofyG00ber <92568429+GoofyG00ber@users.noreply.github.com>
2024-05-18 09:43:27 +0200

    Update tablak_create.sql

--- tablak_create.sql
+++ tablak_create.sql
@@ -1,46 +1,66 @@
 CREATE TABLE Eladasok
 (
-  id int IDENTITY(1,1) PRIMARY KEY,
-  car_id int,
-  customer_id int,
-  employee_id int,
+  id integer not null IDENTITY(1,1) PRIMARY KEY,
+  car_FK integer not null,
+  customer_FK integer not null,
+  employee_FK integer not null,
   eladas_datum date,
 )
 
 CREATE TABLE Autók
 (
-  id int IDENTITY(1,1) PRIMARY KEY,
+  car_id integer not null IDENTITY(1,1) PRIMARY KEY,
   car_name nvarchar(50),
-  prod_year int,
-  color_id int,
+  prod_year integer,
+  color_FK integer not null,
   description nvarchar(300),
-  price int,
+  price integer not null,
 )
 
+ALTER TABLE Eladasok
+    ADD CONSTRAINT eladasok_autok_FK FOREIGN KEY ( car_FK )
+        REFERENCES Autók ( car_id );
+
 CREATE Table Vevők (
-  id int IDENTITY(1,1) PRIMARY KEY,
+  customer_id integer not null IDENTITY(1,1) PRIMARY KEY,
   fname varchar(50),
   lname varchar(50),
-  tel varchar(20),
+  tel integer not null,
   email varchar(50),
   dob date,
 )
 
+ALTER TABLE Eladasok
+    ADD CONSTRAINT eladasok_vevok_FK FOREIGN KEY ( customer_FK )
+        REFERENCES Vevők ( customer_id );
+
 CREATE Table Employee (
-  id int IDENTITY(1,1) PRIMARY KEY,
+  employee_id integer IDENTITY(1,1) PRIMARY KEY,
   fname varchar(50),
   lname varchar(50),
   tel varchar(20),
   email varchar(50),
-  job_id int,
+  job_FK integer not null,
 )
 
+ALTER TABLE Eladasok
+    ADD CONSTRAINT eladasok_employee_FK FOREIGN KEY ( employee_FK )
+        REFERENCES Employee ( employee_id );
+
 CREATE Table Colors (
-  id int IDENTITY(1,1) PRIMARY KEY,
+  color_id integer not null IDENTITY(1,1) PRIMARY KEY,
   color varchar(30),
 )
 
+ALTER TABLE Autók
+    ADD CONSTRAINT car_colors_FK FOREIGN KEY ( color_FK )
+        REFERENCES Color ( color_id );
+
 CREATE Table Jobs (
-  id int IDENTITY(1,1) PRIMARY KEY,
+  job_id integer not null IDENTITY(1,1) PRIMARY KEY,
   job_title varchar(30),
 )
+
+ALTER TABLE Employee
+    ADD CONSTRAINT jobs_employee_FK FOREIGN KEY ( employee_FK )
+        REFERENCES Jobs ( job_id );
